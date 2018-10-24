b97a1acff555fcc6194c0d915ff76a3406085823
5966868 <5966868@U702833.univali.br>
2018-10-24 21:49:14 -0200

    classe TronRunner

---
+++ src/TronRunner.java
@@ -0,0 +1,15 @@
+import jade.core.Agent;
+import java.awt.Color;
+import java.awt.geom.Point2D;
+
+public class TronRunner extends Agent {
+    private Color color;
+    private Point2D position;
+    
+    public TronRunner() {
+        
+    }
+    
+    public void move() {
+    }
+}
